7eaa8fa5e0824b1c946b63d2f1d98127c07f2c44
Marc O'Morain <marc@circleci.com>
2021-03-09 13:31:09 +0000

    JUnit reporter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
+require "minitest/reporters"
+Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new, Minitest::Reporters::JUnitReporter.new]
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
